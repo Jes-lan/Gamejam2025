df6c863bc507cb64731e28ffa16ec0c751ac397e
Jes-lan <95225927+Jes-lan@users.noreply.github.com>
2025-04-25 19:22:46 +0300

    Add player movement and input handling, implement camera zoom, and create mouse character scene

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,23 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var SPEED = 300.0
+var JUMP_VELOCITY = -400.0
 
-
-func _physics_process(delta: float) -> void:
+func movement(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jumping") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+
+	# Change speed to run.
+	if Input.is_action_just_pressed("rolling"):
+		SPEED = 600.0
+	elif Input.is_action_just_released("rolling"):
+		SPEED = 300.0
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -22,4 +27,8 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+
+func _physics_process(delta: float) -> void:
+	movement(delta)
+	# Move the character.
 	move_and_slide()
